8db7f916962b511b8cc7efd2acf5e9ecc6102c48
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-07-12 17:02:34 -0300

    Update

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -6,10 +6,16 @@ export default function Repos() {
 
   return(
     <div
-    className=' text-white'
+    className=' text-white p-5'
     >
-       <Link href={`/`}>
-                Voltar para o consultor de repositórios.
+       <Link href={`/`}
+       className='bg-[#242b66]  p-3 rounded-lg cursor-pointer'
+       >
+        <span
+        className='text-[#9da5d1]'
+        >
+        Voltar 
+        </span>
           </Link>
 
       <Repositorios/>
--- src/app/components/Repositorios.tsx
+++ src/app/components/Repositorios.tsx
@@ -1,6 +1,9 @@
 'use client'
 import React, {useState, useEffect} from 'react'
 import { usePathname  } from 'next/navigation'
+import Link from 'next/link';
+
+import { AiOutlineStar } from 'react-icons/ai';
 
 
 type APi = {
@@ -43,20 +46,60 @@ export const Repositorios = () => {
                    }
         {
             repositorio.map((val) => {
-                return(
-                    <div
-                    key={val.id}
-                    className='bg-slate-700  p-8 md:mx-96 rounded-lg '
-                    >       {/* 
-                    md:p-28
-                     */}
+              return (
+                <div key={val.id} className='bg-[#242b66] p-8 md:mx-96
+                                               rounded-lg mx-auto'>
+                  <div className='bg-[#0e1129] p-20 rounded-lg border-2 flex 
+                                    flex-col text-center space-y-3 justify-center'>
+                    <p>{val.name}</p>
+              
+                    {/* <p>{val.created_at}</p> */}
+              
+                    {
+                    val.language ? (
+                      <div>
+                        <p>{val.language}</p>
+                      </div>
+                    ) : (
+                      <p>Nenhuma linguagem encontrada</p>
+                    )}
+              
+                    <div className='flex flex-row space-x-3 justify-center items-center'>
+                      <div className='flex flex-row'>
+                        
                         <div
-                        className=' bg-red-900 p-20  rounded-lg border-2 '
+                        className='bg-blue-400 p-1 w-10 h-8 text-center'
                         >
-                        <p>{val.name}</p>
+                        <AiOutlineStar
+                         className='mt-1 '
+                          />
+
                         </div>
+
+                        <div className='p-2 h-8 w-14 border'>
+                          <p
+                          className='mb-2'
+                          >{val.watchers}
+                          </p>
+                        </div>
+                      </div>
+              
+                      <p>{val.forks}</p>
                     </div>
-                )
+              
+                    <div className='flex justify-center'> {/* Adicionando a classe 'flex justify-center' */}
+                      <Link
+                        href={val.html_url}
+                        target='_blank'
+                        className='bg-[#242b66] p-3 rounded-full cursor-pointer w-28'
+                      >
+                        <p className='text-[#9da5d1]'>Ver código</p>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              );
+              
             })
         }
     </div>
